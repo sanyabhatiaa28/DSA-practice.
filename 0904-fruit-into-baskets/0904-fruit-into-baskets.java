2c362e792b1651883bd06cd7b2da6332868ba8c3
sanya bhatia <sanyabhatiaa@gmail.com>
2026-07-25 02:00:16 +0530

    Time: 54 ms (66.97%), Space: 70.4 MB (56.86%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -13,9 +13,7 @@ class Solution {
                 }
                 l++;
             }
-            if(map.size()<=2){
-                maxlen=Math.max(maxlen,r-l+1);
-            }
+            maxlen=Math.max(maxlen,r-l+1);
         }
         return maxlen;
     }
