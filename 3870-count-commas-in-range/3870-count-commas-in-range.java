e40e39e20ae5990ef66fd2533872d41dee858fdb
sanya bhatia <sanyabhatiaa@gmail.com>
2026-09-17 17:17:44 +0530

    Time: 8 ms (17.01%), Space: 42.3 MB (89.06%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int n) {
+        int cnt=0;
+        for(int i=1;i<=n;i++){
+            if(i>=1000){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
